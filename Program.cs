cb3344a0efc91a09b94ef7ab15603ec7ffee7114
jmgallen <35116410+jmgallen@users.noreply.github.com>
2022-09-23 15:46:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// Jeremy Gallen's sample program to demonstrate interfaces
+
+namespace InterfaceExample
+{
+    // Draw shape interface
+    public interface DrawShape
+    {
+        void draw();
+    }
+
+    // Class to draw shape
+    public class Square : DrawShape
+    {
+        // Implementation of draw method
+        public void draw()
+        {
+            Console.WriteLine("Drawing a square...");
+        }
+    }
+
+    // Triangle class
+    public class Triangle : DrawShape
+    {
+        // Implement draw method
+        public void draw()
+        {
+            Console.WriteLine("Drawing a triangle...");
+        }
+    }
+
+    // Main class
+    internal class Program
+    {
+        // Main method
+        static void Main(string[] args)
+        {
+            // Demonstrate interfaces
+            DrawShape shape;
+            shape = new Square();
+            shape.draw();
+            shape = new Triangle();
+            shape.draw();
+        }
+    }
+}
